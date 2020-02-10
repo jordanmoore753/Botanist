3e8800401daf00de06877c2848d3a3a523e593fb
jordanmoore753 <jordanmoore753@gmail.com>
2020-02-10 08:59:09 -0500

    add footer and specific information on collection page;

--- public/javascripts/analysis.js
+++ public/javascripts/analysis.js
@@ -9,11 +9,57 @@ $(function() {
     },
 
     bindEvents: function() {
+      $('#analysis_graph').on('click', '.key-item', $.proxy(this.clickShowDetail, this));
       $('#collection').on('click', 'a', $.proxy(this.analyzePlant, this));
       $('#deleter_btn').on('click', $.proxy(this.deletePlant, this));
       $('#add_viewer_btn').on('click', $.proxy(this.goToAdd, this));
       $('#view_sightings_btn').on('click', $.proxy(this.goToView, this));
       $('#view_notes_btn').on('click', $.proxy(this.goToNotes, this));
+    },
+
+    clickShowDetail: function(e) {
+      e.preventDefault();
+
+      $('#details_info div').remove();
+      $('#details_info p').remove();
+
+      let li;
+      let content = document.createElement('div');
+      content.classList.add('content');
+      $('#details_info').css('height', '325px');
+      $('#details_info').css('overflow', 'auto');
+
+      let ol = document.createElement('ol');
+      ol.style.marginLeft = '0em';
+      ol.style.marginTop = `0`;
+      content.append(ol);
+
+      $('#details_info').append(content);
+      let $items = $(e.target).find('li');
+      let f;
+      let s;
+      let one;
+      let two;
+
+      $items.each(function(i, e) {
+        f = e.textContent.split(':')[0];
+        s = e.textContent.split(':')[1];
+        one = document.createElement('span');
+        one.classList.add('key-class');
+        one.textContent = f;
+        two = document.createElement('span');
+        two.classList.add('desc-class');
+        two.textContent = ': ' + s;
+
+        li = document.createElement('li');
+        li.append(one);
+        li.append(two);
+        li.classList.add('detail-item');
+        $('#details_info div ol').append(li);
+      });
+
+      $('#details_info li').css('display', 'block');
+       // insert all sub a links into separate column
     },
 
     populateDetails: function(plantData) {
@@ -32,47 +78,50 @@ $(function() {
           false: 'No',
           null: 'Information not available.'
         };
+        const inc = ['specifications', 'soils_adaptation', 'seed', 'propagation', 'products', 'growth', 'fruit_or_seed'];
+        const convertsTwo = {
+          inches: 'Precipitation (Low-High): ',
+          deg_f: 'Temperature Minimum: ',
+          ft: 'Height (Max-Mature): ',
+          sqm: 'Planting Density (Min-Max): '
+        };
 
         let div;
         let li;
         let keys = Object.keys(object);
 
         keys.forEach(function(key) {
-          console.log(key);
           if (object[key] === null) {
             return;
-          } else if (typeof object[key] === 'object') {
+          } else if (typeof object[key] === 'object' && inc.includes(key)) {
             let div = document.createElement('a');
 
             div.textContent = noValues.includes(key) ? '' : capitalize(key);
-            div.classList.add('list-item');
+            div.classList.add('list-item', 'key-item');
             parentList.append(div);
 
             getValues(div, object[key]);
+          } else if (typeof object[key] === 'object') {
+            getValues(parentList, object[key]);
           } else {
             if (noConversions.includes(key)) {
               return;
             }
 
-            li = document.createElement('a');
-            li.classList.add('list-item');
+            li = document.createElement('li');
+            li.classList.add('list-item', 'has-text-primary');
 
             if (special.includes(key)) {
-              li.textContent = object[key] + ' ' + capitalize(key);
+              li.textContent = convertsTwo[key] + object[key] + ' ' + capitalize(key);
               li.style.display = 'none';
               parentList.append(li);
               return;
             }
 
-            div = document.createElement('a');
-            div.textContent = capitalize(key);
-            div.classList.add('list-item');
-
-            li.textContent = converts[object[key]] ? converts[object[key]] : object[key];
+            li.textContent = capitalize(key) + ': ';
+            li.textContent += converts[object[key]] ? converts[object[key]] : object[key];
             li.style.display = 'none';
-
-            div.append(li);
-            parentList.append(div);
+            parentList.append(li);
           }
         });
       };
@@ -84,7 +133,6 @@ $(function() {
         firstObject[key] = plantData[key];
       });
 
-      console.log(firstObject);
       let div = document.createElement('div');
       div.classList.add('list', 'is-hoverable');
 
@@ -159,11 +207,25 @@ $(function() {
           $('#collection').append(b);
         }
 
+        $('#analysis_graph div').remove();
+        self.resetDetails();
         return self.newMsg({ success: json.msg });
       })
       .catch(function(err) {
         return self.newMsg({ error: 'That plant could not be deleted.' });
       });
+    },
+
+    resetDetails: function() {
+      $('#details_info p').remove();
+      $('#details_info div').remove();
+      $('#details_info').css('height', 'auto');
+
+      let p = document.createElement('p');
+      p.textContent = 'Information regarding a plant will populate here.';
+      p.classList.add('is-medium', 'has-text-centered');
+
+      $('#details_info').append(p);
     },
 
     newMsg: function(msg) {
@@ -211,7 +273,7 @@ $(function() {
       .then(res => res.json())
       .then(function(json) {
         let plantData = JSON.parse(json.body);
-        console.log(plantData['main_species']);
+        self.resetDetails();
         self.populateDetails(plantData['main_species']);
         //self.createGraph(plantData['main_species']);
       })
